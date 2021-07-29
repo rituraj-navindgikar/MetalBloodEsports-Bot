ea8223201a713d1a1ad133258b31827834b873a2
rituraj-navindgikar <74851393+rituraj-navindgikar@users.noreply.github.com>
2021-07-29 10:10:28 +0530

    Update leaderboard.js

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -38,7 +38,7 @@ module.exports = {
 }
 function chunkz(arr, size){
     var array =[];
-    for(vari =0; i<arr.length; i+=size){
+    for(var i =0; i<arr.length; i+=size){
         array.push(arr.slice(i, i+size))
     }
     return array;
