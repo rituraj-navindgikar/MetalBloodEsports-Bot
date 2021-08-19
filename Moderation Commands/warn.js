ded64d421d6823a2fb756e70d9ca5e3ae97ab94e
rituraj-navindgikar <74851393+rituraj-navindgikar@users.noreply.github.com>
2021-08-19 19:35:02 +0530

    Update warn.js

--- Moderation Commands/warn.js
+++ Moderation Commands/warn.js
@@ -14,10 +14,11 @@ try{
             if(!context) return message.channel.send('Type a message to send')
       
             const contextresult = new Discord.MessageEmbed()
+            .setTitle(`Your role **${role.name}** got a Warn from MTB Team. Moderator - ${message.author.id}`)
             .setColor('#ff0000')
             .setDescription(context)
             .setTimestamp()
-            .setFooter("Warning From MTB Management Team")
+            //.setFooter("Warning From MTB Management Team")
       
             const role = message.mentions.roles.first() 
             if(!role) return message.channel.send('Mention a role')
@@ -83,10 +84,11 @@ try{
             // below correct
             message.channel.send(`Gave your Warning successfully`)
             const contentresult = new Discord.MessageEmbed()
+            .setTitle(`You got a Warn from MTB Team. Moderator - ${message.author.id}`)
             .setColor("#ff0000")
             .setDescription(content)
             .setTimestamp()
-            .setFooter("Warning From MTB Management Team")
+           // .setFooter("Warning From MTB Management Team")
             let interv = setInterval(() => {
 
                 let member = member_Array.splice(Math.floor(Math.random() * member_Array.length), 1).toString();
