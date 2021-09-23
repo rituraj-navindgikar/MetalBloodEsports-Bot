581a9e16216c3c099b3c370ccf012912c1abe165
rituraj-navindgikar <74851393+rituraj-navindgikar@users.noreply.github.com>
2021-09-23 13:30:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -518,10 +518,10 @@ try{
        await channel.send(`<a:redstar:836509946446610453>**Greetings From MTB Esports**<a:redstar:836509946446610453>
        Welcome to our server **${member}**
  Now we are a family of __**${membercount}**__ members <a:hype:759725271245914113>\n
-<a:784339639539335188:854064293217566720> Do remember to take self roles from <#797363384797888543>\n
-<a:784339639539335188:854064293217566720> Chat With our chatbot <#794552577835794442>\n
-<a:784339639539335188:854064293217566720> Join our giveaways in <#843734479365472266>\n
-<a:784339639539335188:854064293217566720>  Have fun in <#750623573395112007>`)+ 
+<a:tick:854064293217566720> Do remember to take self roles from <#797363384797888543>\n
+<a:tick:854064293217566720> Chat With our chatbot <#794552577835794442>\n
+<a:tick:854064293217566720> Join our giveaways in <#843734479365472266>\n
+<a:tick:854064293217566720>  Have fun in <#750623573395112007>`)+ 
        
    channel.send('https://images-ext-1.discordapp.net/external/q2E_EVBNiGs7eWcMepNGXhN8Xxa1ylEMai5uiY03pps/https/media.discordapp.net/attachments/712714975352979507/861265762111193128/standard2.gif')
  
