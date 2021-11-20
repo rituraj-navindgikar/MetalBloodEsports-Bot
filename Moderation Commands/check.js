7ea3897ce2f266be98fd336940787ec69712f0f4
rituraj-navindgikar <riturajn1200@gmail.com>
2021-11-21 01:18:19 +0530

    Update check.js

--- Moderation Commands/check.js
+++ Moderation Commands/check.js
@@ -10,10 +10,12 @@ module.exports = {
         let validRoles = new Array()
 
         message.guild.roles.cache.forEach(role => roleIds.push(role.id))
-    
+        const perm_type = args[0]
+        console.log(perm_type)
+        
         for(var i=0; i<roleIds.length; i++){
            let role = message.guild.roles.cache.find(r=> r.id == roleIds[i])
-            if(role.editable && role.permissions.serialize().args[0]){
+            if(role.editable && role.permissions.serialize().perm_type){
                 validRoles.push(role.name)
             }
         }
