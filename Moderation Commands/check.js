625c6f04bf28564351474124e67472414be33071
rituraj-navindgikar <riturajn1200@gmail.com>
2021-11-21 01:13:28 +0530

    Update check.js

--- Moderation Commands/check.js
+++ Moderation Commands/check.js
@@ -3,19 +3,21 @@ module.exports = {
   async execute(message, args, PREFIX){
 
     if(!args){
-        return message.channel.send("Invalid")
-    }
-    let roleIds = new Array()
-    let validRoles = new Array()
+        message.channel.send("Invalid")
+        return
+    }else{
+        let roleIds = new Array()
+        let validRoles = new Array()
 
-    message.guild.roles.cache.forEach(role => roleIds.push(role.id))
+        message.guild.roles.cache.forEach(role => roleIds.push(role.id))
     
-    for(var i=0; i<roleIds.length; i++){
-        let role = message.guild.roles.cache.find(r=> r.id == roleIds[i])
-        if(role.editable && role.permissions.serialize().args){
-            validRoles.push(role.name)
+        for(var i=0; i<roleIds.length; i++){
+           let role = message.guild.roles.cache.find(r=> r.id == roleIds[i])
+            if(role.editable && role.permissions.serialize().args){
+                validRoles.push(role.name)
+            }
+        }
+        message.channel.send(`Following roles have Permission ${args}\n${validRoles}`)
         }
     }
-    message.channel.send(`Following roles have Permission ${args}\n${validRoles}`)
-  }
 }
