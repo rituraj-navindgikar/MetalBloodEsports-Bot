eeb1ef04540ac11e786dadf2f904a80e0e89376a
rituraj-navindgikar <riturajn1200@gmail.com>
2021-11-21 01:39:47 +0530

    Update check.js

--- Moderation Commands/check.js
+++ Moderation Commands/check.js
@@ -17,7 +17,7 @@ module.exports = {
                     validRoles.push(role.name)
                 }
             }
-            message.channel.send(`Following roles have Permission Admin\n${validRoles.join('\n')}`)
+            message.channel.send(`Following roles have Permission Admin\n**${validRoles.join('\n')}**`)
         }
         else if(args[0] == 'kick'){
             for(var i=0; i<roleIds.length; i++){
@@ -26,13 +26,52 @@ module.exports = {
                     validRoles.push(role.name)
                 }
             }
-            message.channel.send(`Following roles have Permission to Kick\n${validRoles.join('\n')}`)
+            message.channel.send(`Following roles have Permission to Kick\n**${validRoles.join('\n')}**`)
         }
-        else if(args[0] == 'ban'){}
-        else if(args[0] == 'channel'){}
-        else if(args[0] == 'server'){}
-        else if(args[0] == 'role'){}
-        else if(args[0] == 'message'){}
+        else if(args[0] == 'ban'){
+            for(var i=0; i<roleIds.length; i++){
+                let role = message.guild.roles.cache.find(r=> r.id == roleIds[i])
+                 if(role.editable && role.permissions.serialize().BAN_MEMBERS){
+                    validRoles.push(role.name)
+                }
+            }
+            message.channel.send(`Following roles have Permission to Ban\n**${validRoles.join('\n')}**`)
+        }
+        else if(args[0] == 'channel'){            
+            for(var i=0; i<roleIds.length; i++){
+            let role = message.guild.roles.cache.find(r=> r.id == roleIds[i])
+             if(role.editable && role.permissions.serialize().MANAGE_CHANNELS){
+                validRoles.push(role.name)
+            }
+        }
+        message.channel.send(`Following roles have Permission to Manage Channel\n**${validRoles.join('\n')}**`)}
+        else if(args[0] == 'server'){
+            for(var i=0; i<roleIds.length; i++){
+                let role = message.guild.roles.cache.find(r=> r.id == roleIds[i])
+                 if(role.editable && role.permissions.serialize().MANAGE_GUILD){
+                    validRoles.push(role.name)
+                }
+            }
+            message.channel.send(`Following roles have Permission to Manage Server\n**${validRoles.join('\n')}**`)
+        }
+        else if(args[0] == 'role'){
+            for(var i=0; i<roleIds.length; i++){
+                let role = message.guild.roles.cache.find(r=> r.id == roleIds[i])
+                 if(role.editable && role.permissions.serialize().MANAGE_ROLES){
+                    validRoles.push(role.name)
+                }
+            }
+            message.channel.send(`Following roles have Permission to Manage roles\n**${validRoles.join('\n')}**`)
+        }
+        else if(args[0] == 'message'){
+            for(var i=0; i<roleIds.length; i++){
+                let role = message.guild.roles.cache.find(r=> r.id == roleIds[i])
+                 if(role.editable && role.permissions.serialize().MANAGE_MESSAGES){
+                    validRoles.push(role.name)
+                }
+            }
+            message.channel.send(`Following roles have Permission to Manage Messages\n**${validRoles.join('\n')}**`)
+        }
         else{return message.channel.send("Wrong input correct input should be any one from below\n**admin**\n**kick**\n**ban**\n**channel**\n**server**\n**role**\n**message**")}
         
         return
